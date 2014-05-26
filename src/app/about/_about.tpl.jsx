14ccf2d06daf003ec3c3fd418f149a8759b3edcf
ng-vu <ng-vu@liti.ws>
2014-05-26 16:23:11 +0700

    Update content

--- src/app/about/_about.tpl.jsx
+++ src/app/about/_about.tpl.jsx
@@ -1,3 +1,58 @@
-<div>
-  This is about page
+<div class="about">
+  <h3>About Mithril-Boilerplate</h3>
+  <p>
+    The project is a well structured boilerplate code for quickly starting new Mithril.js projects.<br/>
+    For more information, browse <a href="https://github.com/ng-vu/mithril-boilerplate">project page</a> on GitHub.
+  </p>
+  <h3>Features</h3>
+  <p>
+    <ul>
+      <li>Gulpfile</li>
+      <li>Bower</li>
+      <li>LESS & Bootstrap (easy to swap with your favourite tools)</li>
+      <li>JSX (write HTML tags inside .jsx file)</li>
+      <li>INCLUDE() JavaScript files</li>
+      <li>Client side modules wrapped in CommonJs</li>
+      <li>Watch with cache (only rebuild changed files)</li>
+      <li>LiveReload</li>
+      <li>Concat and minify stylesheets and scripts for production</li>
+      <li>Unit testing will be supported soon</li>
+    </ul>
+  </p>
+  <h3>Quick start</h3>
+  <p>
+    Make sure you have node and npm installed. Then
+    <pre><code>
+    npm install -g bower gulp http-server<br/><br/>
+
+    git clone https://github.com/ng-vu/mithril-boilerplate.git<br/>
+    cd mithril-boilerplate<br/><br/>
+
+    npm install<br/>
+    bower install<br/>
+    gulp compile<br/>
+
+    http-server build/public
+    </code></pre>
+  </p>
+  <p>
+    Running above commands compile source and put final code in these directories:
+    <ul>
+      <li><code>build</code> for development</li>
+      <li><code>bin</code> for production</li>
+    </ul>
+  </p>
+  <p>
+    You may run the application by either of these commands:
+    <pre><code>
+    http-server build/public<br/>
+    http-server bin/public
+    </code></pre>
+  </p>
+  <p>
+    To automatically build project whenever a source file changes:
+    <pre><code>
+    gulp build watch
+    </code></pre>
+  </p>
 </div>
--- src/app/home/_home.tpl.jsx
+++ src/app/home/_home.tpl.jsx
@@ -1,3 +1,43 @@
+[
+<div class="row">
+  <div class="col-md-8">
+    <h3>What is Mithril?</h3>
+    <p>
+      Mithril is a client-side MVC framework - a tool to organize code in a way that is easy to think about and to maintain.<br/>
+      For more information, go to <a href="http://lhorie.github.io/">official website</a> or read <a href="http://lhorie.github.io/mithril/getting-started.html">Getting Started</a>.
+    </p>
+  </div>
+</div>,
+<hr/>,
+<div class="row">
+  <div class="col-md-4">
+    <h3>Light-weight</h3>
+    <p>
+      Only 3kb gzipped, no dependencies<br/>
+      Small API, small learning curve
+    </p>
+  </div>
+  <div class="col-md-4">
+    <h3>Robust</h3>
+    <p>
+      Safe-by-default templates<br/>
+      Hierarchical MVC via components
+    </p>
+  </div>
+  <div class="col-md-4">
+    <h3>Fast</h3>
+    <p>
+      Virtual DOM diffing and compilable templates<br/>
+      Intelligent auto-redrawing system
+    </p>
+  </div>
+</div>,
+<hr/>,
+<div class="row">
+  <div class="col-md-8">
+    <h3>Examples</h3>
+  </div>
+</div>,
 <div class="row">
   <div class="col-md-6 description">
     {description()}
@@ -6,3 +46,4 @@
     {new ctrl.tabs.$view(ctrl.tabsCtrl)}
   </div>
 </div>
+]
